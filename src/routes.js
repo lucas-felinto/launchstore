4082d7ff117427779d0d3f1fb6ffe32521bfce36
lucas-felinto <lucasfepinto@gmail.com>
2020-05-19 18:43:04 -0300

    feat form

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const productsController = require('./app/controllers/productController')
 
 const routes = express.Router()
 
 routes.get("/", function (req, res) {
     return res.render("layout")
+})
+
+routes.get("/products/create", productsController.create)
+
+routes.get('/ads/create', function (req, res) {
+    return res.redirect("/products/create")
 })
 
 module.exports = routes
